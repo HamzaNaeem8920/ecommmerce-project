911b0ab264e56fa5850be9683cd321a5cca3f46a
HamzaNaeem8920 <hnaeem8920@gmail.com>
2023-05-17 11:54:14 +0500

    added authentication

--- routes/authroute.js
+++ routes/authroute.js
@@ -1,4 +1,5 @@
 import { Express } from "express";
+import  {registerController}  from "../controllers/authcontroller.js";
 
 // router object 
 const router = express.Router()
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import  express from 'express'
 import dotenv, { config } from 'dotenv'
 import morgan from 'morgan';
 import { connectDB } from './config/db.js';
-
+import authRoutes from './routes/authroute.js'
 
 dotenv.config();
 
@@ -17,6 +17,9 @@ connectDB();
 
 app.use(express.json())
 app.use(morgan('dev'))
+
+// routes 
+app.use('/api/v1/auth', authRoutes)
 
 // rest api 
 app.get('/',(request, response) => {
